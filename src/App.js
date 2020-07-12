a79cce946707d68958e3081e06edf11823d2e6c3
Jayman Gandhi <jayman.gandhi@gmail.com>
2020-07-12 09:52:01 -0400

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
 import './App.css';
+import TableTop from 'tabletop';
 
 function App() {
+    const[data, setData] = useState([]);
+
+    useEffect(()=> {
+      TableTop.init({
+        key: '1d-Gbh8R9sc4DwT2lFPUGQ4EToBOGpUQNm16_O45fO0w',
+        callback: googleData => {
+          setData(googleData)
+        },
+        simpleSheet: true
+      })
+    }, [])
+    console.log(data);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          React + Google Sheet Demo
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div id="employee-detail">
+      {
+            data.map(obj => {
+              return (
+                <div key={obj.employee}>
+                  <p>{obj.employee}</p>
+                  <p>{obj.favDog}</p>
+                  <img alt={obj.favDog} src={obj.img} />
+                </div>
+              )
+            })
+          }
+      </div>
+  </div>
   );
 }
 
